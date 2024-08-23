e0436ab6548908e614eef36ab8b32003268fe5de
kuznetsovt20 <63197933+kuznetsovt20@users.noreply.github.com>
2024-08-23 10:17:05 -0600

    commit6

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-import time
-
 from game import Game
+from gpiozero import Button
 
 g = Game()
+buttonNext, buttonBack, buttonDown, buttonUp = Button(27), Button(17), Button(22), Button(23)
 
 while g.running:
     g.curr_menu.display_menu()
     g.transfer_loop()
+
+    if buttonNext.is_pressed:
+        g.self.START_KEY = True
+    if buttonBack.is_pressed:
+        g.self.BACK_KEY = True
+    if buttonDown.is_pressed:
+        g.self.DOWN_KEY = True
+    if buttonUp.is_pressed:
+        g.self.UP_KEY = True
